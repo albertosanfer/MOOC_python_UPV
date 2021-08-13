38a5677a6f4f2f2dad0609ca1616925faa70edf7
Alberto Sánchez Fernández <alb.sanfer@gmail.com>
2021-08-13 22:53:15 +0200

    Modificación de línea a 79 caracteres

--- Módulo 1/Práctica1_1.py
+++ Módulo 1/Práctica1_1.py
@@ -1,3 +1,4 @@
-# Utilizando el comando print(), escribe un código que muestre por pantalla el texto 'hola mundo
+# Utilizando el comando print(), escribe un código que muestre por pantalla el
+# texto 'hola mundo
 
 print("Hola mundo")
